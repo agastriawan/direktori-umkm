139805b2866af0a0c389b7d2fc481a7b914b0cb8
Agas Triawan <agastriawan@gmail.com>
2024-07-12 12:53:38 +0700

    perbaikan header

--- resources/views/admin/template_admin/header.blade.php
+++ resources/views/admin/template_admin/header.blade.php
@@ -2,7 +2,6 @@
     <div class="container-xl">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu"
             aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
         </button>
         <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
         </h1>
